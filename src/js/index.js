f1d6964135fe30f939560b4e2b3a06351c8140ed
Jessie <miss_sc_jessi@hotmail.com>
2022-08-01 16:17:44 -0300

    create project

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+const bntAvancar = document.getElementById("btn-avancar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+}
+
+function mostrarCartao(indiceCartao) {
+  cartoes[indiceCartao].classList.add("selecionado");
+}
+
+bntAvancar.addEventListener("click", function () {
+  if (cartaoAtual === cartoes.length - 1) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual);
+});
+
+const bntVoltar = document.getElementById("btn-voltar");
+
+bntVoltar.addEventListener("click", function () {
+  if (cartaoAtual === 0) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
+});
